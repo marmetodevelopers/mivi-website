62789432c5effde4ac6312d3ba56237a83b44d63
NikhitaDesai <145182249+NikhitaDesai@users.noreply.github.com>
2023-11-08 12:35:16 +0530

    updated changes

--- assets/global.js
+++ assets/global.js
@@ -1257,22 +1257,7 @@ class ProductRecommendations extends HTMLElement {
 }
 
 customElements.define('product-recommendations', ProductRecommendations);
-
-
-const hoursEl = document.getElementById('hours');
-const minsEL = document.getElementById('mins');
-const newYears = document.querySelector('.text-white').getAttribute('data-time');
-function countdown() {
-  const newYearsDate = new Date(newYears);
-  const currentDate = new Date();
-
-  const totalSeconds = (newYearsDate - currentDate) / 1000;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  hoursEl.innerText = hours;
-  minsEL.innerText = minutes;
-}
-setInterval(countdown, 1000);
+ 
 
 // footer for mobile
 document.querySelectorAll('.footer__blocks-wrapper .footer__trigger-accordion').forEach((each) => {
