3c1055baa6548463286d5bb14eb78716b73cfe0d
Nehal Ahamed <nehal.a@marmeto.com>
2024-01-18 19:32:55 +0530

    bug fixes 180124

--- assets/custom-components.js
+++ assets/custom-components.js
@@ -1,9 +1,9 @@
 class MarmetoProductCard extends HTMLElement {
   constructor() {
     super();
-    this.addEventListener('change', event => {
-      if(event.target.name == "id") this.variantChange();
-    })
+    this.addEventListener("change", (event) => {
+      if (event.target.name == "id") this.variantChange();
+    });
   }
   variantChange() {
     this.selectedVariantId = this.querySelector('[name="id"]').value;
@@ -13,29 +13,32 @@ class MarmetoProductCard extends HTMLElement {
     fetch(variantUrl)
       .then((response) => response.text())
       .then((responseText) => {
-        const html = new DOMParser().parseFromString(responseText, 'text/html');
-        const responseCard = html.querySelector('marmeto-product-card');
+        const html = new DOMParser().parseFromString(responseText, "text/html");
+        const responseCard = html.querySelector("marmeto-product-card");
         this.innerHTML = responseCard.innerHTML;
-        this.querySelector('marmeto-product-card .option-button_product').classList.add('selected');
+        console.log(responseCard.innerHTML);
+        this.querySelector(
+          "marmeto-product-card .option-button_product"
+        ).classList.add("selected");
       });
   }
 }
-customElements.define('marmeto-product-card', MarmetoProductCard)
+customElements.define("marmeto-product-card", MarmetoProductCard);
 
 class MMvariantSelects extends HTMLElement {
   constructor() {
     super();
-    this.addEventListener('change', this.onVariantChange);
+    this.addEventListener("change", this.onVariantChange);
   }
 
   onVariantChange() {
     this.updateOptions();
     this.updateMasterId();
-    this.toggleAddButton(true, '', false);
+    this.toggleAddButton(true, "", false);
     this.updateVariantStatuses();
 
     if (!this.currentVariant) {
-      this.toggleAddButton(true, '', true);
+      this.toggleAddButton(true, "", true);
       this.setUnavailable();
     } else {
       this.updateVariantInput();
@@ -43,60 +46,83 @@ class MMvariantSelects extends HTMLElement {
   }
 
   updateOptions() {
-    this.options = Array.from(this.querySelectorAll('select'), (select) => select.value);
+    this.options = Array.from(
+      this.querySelectorAll("select"),
+      (select) => select.value
+    );
   }
 
   updateMasterId() {
     this.currentVariant = this.getVariantData().find((variant) => {
-      return !variant.options.map((option, index) => {
-        return this.options[index] === option;
-      }).includes(false);
+      return !variant.options
+        .map((option, index) => {
+          return this.options[index] === option;
+        })
+        .includes(false);
     });
   }
 
   updateVariantInput() {
-    const productForms = document.querySelectorAll(`#mm-product-form--${ this.dataset.pid }`);
+    const productForms = document.querySelectorAll(
+      `#mm-product-form--${this.dataset.pid}`
+    );
     productForms.forEach((productForm) => {
       const input = productForm.querySelector('input[name="id"]');
       input.value = this.currentVariant.id;
-      input.dispatchEvent(new Event('change', { bubbles: true }));
+      input.dispatchEvent(new Event("change", { bubbles: true }));
     });
   }
 
   updateVariantStatuses() {
-    const selectedOptionOneVariants = this.variantData.filter(variant => this.querySelector(':checked').value === variant.option1);
-    const inputWrappers = [...this.querySelectorAll('.product-form__input')];
+    const selectedOptionOneVariants = this.variantData.filter(
+      (variant) => this.querySelector(":checked").value === variant.option1
+    );
+    const inputWrappers = [...this.querySelectorAll(".product-form__input")];
     inputWrappers.forEach((option, index) => {
       if (index === 0) return;
-      const optionInputs = [...option.querySelectorAll('input[type="radio"], option')]
-      const previousOptionSelected = inputWrappers[index - 1].querySelector(':checked').value;
-      const availableOptionInputsValue = selectedOptionOneVariants.filter(variant => variant.available && variant[`option${ index }`] === previousOptionSelected).map(variantOption => variantOption[`option${ index + 1 }`]);
-      this.setInputAvailability(optionInputs, availableOptionInputsValue)
+      const optionInputs = [
+        ...option.querySelectorAll('input[type="radio"], option'),
+      ];
+      const previousOptionSelected =
+        inputWrappers[index - 1].querySelector(":checked").value;
+      const availableOptionInputsValue = selectedOptionOneVariants
+        .filter(
+          (variant) =>
+            variant.available &&
+            variant[`option${index}`] === previousOptionSelected
+        )
+        .map((variantOption) => variantOption[`option${index + 1}`]);
+      this.setInputAvailability(optionInputs, availableOptionInputsValue);
     });
   }
 
   setInputAvailability(listOfOptions, listOfAvailableOptions) {
-    listOfOptions.forEach(input => {
-      if (listOfAvailableOptions.includes(input.getAttribute('value'))) {
-        input.innerText = input.getAttribute('value');
+    listOfOptions.forEach((input) => {
+      if (listOfAvailableOptions.includes(input.getAttribute("value"))) {
+        input.innerText = input.getAttribute("value");
       } else {
-        input.innerText = window.variantStrings.unavailable_with_option.replace('[value]', input.getAttribute('value'));
+        input.innerText = window.variantStrings.unavailable_with_option.replace(
+          "[value]",
+          input.getAttribute("value")
+        );
       }
     });
   }
 
   toggleAddButton(disable = true, text, modifyClass = true) {
-    const productForm = document.getElementById(`mm-product-form--${this.dataset.pid}`);
+    const productForm = document.getElementById(
+      `mm-product-form--${this.dataset.pid}`
+    );
     if (!productForm) return;
     const addButton = productForm.querySelector('[name="add"]');
     const addButtonText = productForm.querySelector('[name="add"] > span');
     if (!addButton) return;
 
     if (disable) {
-      addButton.setAttribute('disabled', 'disabled');
+      addButton.setAttribute("disabled", "disabled");
       if (text) addButtonText.textContent = text;
     } else {
-      addButton.removeAttribute('disabled');
+      addButton.removeAttribute("disabled");
       addButtonText.textContent = window.variantStrings.addToCart;
     }
 
@@ -104,23 +130,27 @@ class MMvariantSelects extends HTMLElement {
   }
 
   setUnavailable() {
-    const button = document.getElementById(`mm-product-form--${this.dataset.pid}`);
+    const button = document.getElementById(
+      `mm-product-form--${this.dataset.pid}`
+    );
     const addButton = button.querySelector('[name="add"]');
     const addButtonText = button.querySelector('[name="add"] > span');
     const price = document.getElementById(`price-${this.dataset.pid}`);
 
     if (!addButton) return;
     addButtonText.textContent = window.variantStrings.unavailable;
-    if (price) price.classList.add('visibility-hidden');
+    if (price) price.classList.add("visibility-hidden");
   }
 
   getVariantData() {
-    this.variantData = this.variantData || JSON.parse(this.querySelector('[type="application/json"]').textContent);
+    this.variantData =
+      this.variantData ||
+      JSON.parse(this.querySelector('[type="application/json"]').textContent);
     return this.variantData;
   }
 }
 
-customElements.define('mm-variant-selects', MMvariantSelects);
+customElements.define("mm-variant-selects", MMvariantSelects);
 
 class MMvariantRadios extends MMvariantSelects {
   constructor() {
@@ -128,58 +158,72 @@ class MMvariantRadios extends MMvariantSelects {
   }
 
   setInputAvailability(listOfOptions, listOfAvailableOptions) {
-    listOfOptions.forEach(input => {
-      if (listOfAvailableOptions.includes(input.getAttribute('value'))) {
-        input.classList.remove('disabled');
+    listOfOptions.forEach((input) => {
+      if (listOfAvailableOptions.includes(input.getAttribute("value"))) {
+        input.classList.remove("disabled");
       } else {
-        input.classList.add('disabled');
+        input.classList.add("disabled");
       }
     });
   }
 
   updateOptions() {
-    const fieldsets = Array.from(this.querySelectorAll('fieldset'));
+    const fieldsets = Array.from(this.querySelectorAll("fieldset"));
     this.options = fieldsets.map((fieldset) => {
-      return Array.from(fieldset.querySelectorAll('input')).find((radio) => radio.checked).value;
+      return Array.from(fieldset.querySelectorAll("input")).find(
+        (radio) => radio.checked
+      ).value;
     });
   }
 }
 
-customElements.define('mm-variant-radios', MMvariantRadios);
+customElements.define("mm-variant-radios", MMvariantRadios);
 
- customElements.define('mm-product-form', class ProductForm extends HTMLElement {
+customElements.define(
+  "mm-product-form",
+  class ProductForm extends HTMLElement {
     constructor() {
       super();
 
-      this.form = this.querySelector('form');
-      this.form.querySelector('[name=id]').disabled = false;
-      this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
-      this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+      this.form = this.querySelector("form");
+      this.form.querySelector("[name=id]").disabled = false;
+      this.form.addEventListener("submit", this.onSubmitHandler.bind(this));
+      this.cart =
+        document.querySelector("cart-notification") ||
+        document.querySelector("cart-drawer");
       this.submitButton = this.querySelector('[type="submit"]');
-      if (document.querySelector('cart-drawer')) this.submitButton.setAttribute('aria-haspopup', 'dialog');
+      if (document.querySelector("cart-drawer"))
+        this.submitButton.setAttribute("aria-haspopup", "dialog");
     }
 
     async onSubmitHandler(evt) {
       evt.preventDefault();
-      if (this.submitButton.getAttribute('aria-disabled') === 'true') return;
+      if (this.submitButton.getAttribute("aria-disabled") === "true") return;
 
       this.handleErrorMessage();
 
-      this.submitButton.setAttribute('aria-disabled', true);
-      this.submitButton.classList.add('loading');
-      this.querySelector('.loading-overlay__spinner').classList.remove('hidden');
+      this.submitButton.setAttribute("aria-disabled", true);
+      this.submitButton.classList.add("loading");
+      this.querySelector(".loading-overlay__spinner").classList.remove(
+        "hidden"
+      );
 
-      const config = fetchConfig('javascript');
-      config.headers['X-Requested-With'] = 'XMLHttpRequest';
-      delete config.headers['Content-Type'];
+      const config = fetchConfig("javascript");
+      config.headers["X-Requested-With"] = "XMLHttpRequest";
+      delete config.headers["Content-Type"];
 
       let sectionsToBundle = [];
-      document.documentElement.dispatchEvent(new CustomEvent("cart:prepare-bundled-sections", { bubbles: true, detail: { sections: sectionsToBundle } }));
+      document.documentElement.dispatchEvent(
+        new CustomEvent("cart:prepare-bundled-sections", {
+          bubbles: true,
+          detail: { sections: sectionsToBundle },
+        })
+      );
 
       const formData = new FormData(this.form);
       if (this.cart) {
-        formData.append('sections', sectionsToBundle.join(","));
-        formData.append('sections_url', window.location.pathname);
+        formData.append("sections", sectionsToBundle.join(","));
+        formData.append("sections_url", window.location.pathname);
       }
       config.body = formData;
 
@@ -189,11 +233,12 @@ customElements.define('mm-variant-radios', MMvariantRadios);
           if (response.status) {
             this.handleErrorMessage(response.description);
 
-            const soldOutMessage = this.submitButton.querySelector('.sold-out-message');
+            const soldOutMessage =
+              this.submitButton.querySelector(".sold-out-message");
             if (!soldOutMessage) return;
-            this.submitButton.setAttribute('aria-disabled', true);
-            this.submitButton.querySelector('span').classList.add('hidden');
-            soldOutMessage.classList.remove('hidden');
+            this.submitButton.setAttribute("aria-disabled", true);
+            this.submitButton.querySelector("span").classList.add("hidden");
+            soldOutMessage.classList.remove("hidden");
             this.error = true;
             return;
           } else if (!this.cart) {
@@ -201,55 +246,79 @@ customElements.define('mm-variant-radios', MMvariantRadios);
             return;
           }
           this.error = false;
-          const quickAddModal = this.closest('quick-add-modal');
+          const quickAddModal = this.closest("quick-add-modal");
           if (quickAddModal) {
-            document.body.addEventListener('modalClosed', () => {
-              setTimeout(() => { this.cart.renderContents(response) });
-            }, { once: true });
+            document.body.addEventListener(
+              "modalClosed",
+              () => {
+                setTimeout(() => {
+                  this.cart.renderContents(response);
+                });
+              },
+              { once: true }
+            );
             quickAddModal.hide(true);
           } else {
-              this.updateCart(response)
+            this.updateCart(response);
           }
         })
         .catch((e) => {
           console.error(e);
         })
         .finally(() => {
-          this.submitButton.classList.remove('loading');
-          if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
-          if (!this.error) this.submitButton.removeAttribute('aria-disabled');
-          this.querySelector('.loading-overlay__spinner').classList.add('hidden');
+          this.submitButton.classList.remove("loading");
+          if (this.cart && this.cart.classList.contains("is-empty"))
+            this.cart.classList.remove("is-empty");
+          if (!this.error) this.submitButton.removeAttribute("aria-disabled");
+          this.querySelector(".loading-overlay__spinner").classList.add(
+            "hidden"
+          );
         });
     }
 
-    async updateCart(responseJson){
-      const cartContent = await fetch(`${Shopify.routes.root}cart.js`).then((res)=> res.json()).then((data)=> {return data});
+    async updateCart(responseJson) {
+      const cartContent = await fetch(`${Shopify.routes.root}cart.js`)
+        .then((res) => res.json())
+        .then((data) => {
+          return data;
+        });
       cartContent["sections"] = responseJson["sections"];
-      this.dispatchEvent(new CustomEvent("variant:add", {
-        bubbles: true,
-        detail: {
-          items: responseJson.hasOwnProperty("items") ? responseJson["items"] : [responseJson],
-          cart: cartContent
-        }
-      }));
-      document.documentElement.dispatchEvent(new CustomEvent("cart:change", {
-        bubbles: true,
-        detail: {
-          baseEvent: "variant:add",
-          cart: cartContent
-        }
-      }));
+      this.dispatchEvent(
+        new CustomEvent("variant:add", {
+          bubbles: true,
+          detail: {
+            items: responseJson.hasOwnProperty("items")
+              ? responseJson["items"]
+              : [responseJson],
+            cart: cartContent,
+          },
+        })
+      );
+      document.documentElement.dispatchEvent(
+        new CustomEvent("cart:change", {
+          bubbles: true,
+          detail: {
+            baseEvent: "variant:add",
+            cart: cartContent,
+          },
+        })
+      );
     }
 
     handleErrorMessage(errorMessage = false) {
-      this.errorMessageWrapper = this.errorMessageWrapper || this.querySelector('.product-form__error-message-wrapper');
+      this.errorMessageWrapper =
+        this.errorMessageWrapper ||
+        this.querySelector(".product-form__error-message-wrapper");
       if (!this.errorMessageWrapper) return;
-      this.errorMessage = this.errorMessage || this.errorMessageWrapper.querySelector('.product-form__error-message');
+      this.errorMessage =
+        this.errorMessage ||
+        this.errorMessageWrapper.querySelector(".product-form__error-message");
 
-      this.errorMessageWrapper.toggleAttribute('hidden', !errorMessage);
+      this.errorMessageWrapper.toggleAttribute("hidden", !errorMessage);
 
       if (errorMessage) {
         this.errorMessage.textContent = errorMessage;
       }
     }
-  });
+  }
+);
